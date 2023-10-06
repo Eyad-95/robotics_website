730c64025c6b2f039a86d7e889c48e637ed7b858
Eyad Hussein <eyadhussein952018@gmail.com>
2023-10-06 18:23:30 +0300

    done with navigation collapsed and competitions page(responsiveness remains)

--- js/script.js
+++ js/script.js
@@ -3,6 +3,24 @@ const slide = document.querySelector(".carousel__img-box");
 const prevButton = document.getElementById("slide-arrow-prev");
 const nextButton = document.getElementById("slide-arrow-next");
 
+const toggleNavButton = document.getElementById("toggle-nav-button");
+const navCollapsed = document.getElementById("nav-collapsed");
+const navCollapsedNavList = document.getElementById("nav-collapsed__nav-list");
+
+const toggleNavButtonFunc = () => {
+  toggleNavButton.classList.toggle("ham-btn--toggled");
+
+  if (toggleNavButton.classList.contains("ham-btn--toggled")) {
+    toggleNavButton.style.position = "fixed";
+    navCollapsed.style.transform = "translateX(0%)";
+    navCollapsedNavList.style.transform = "translateX(0%)";
+  } else {
+    toggleNavButton.style.position = "absolute";
+    navCollapsed.style.transform = "translateX(-100%)";
+    navCollapsedNavList.style.transform = "translateX(-100%)";
+  }
+};
+
 // index.html -- carousel functionality
 nextButton.addEventListener("click", () => {
   const slideWidth = slide.clientWidth;
@@ -13,3 +31,6 @@ prevButton.addEventListener("click", () => {
   const slideWidth = slide.clientWidth;
   carouselContainer.scrollLeft -= slideWidth;
 });
+
+// toggle of hambtn
+toggleNavButton.addEventListener("click", toggleNavButtonFunc);
