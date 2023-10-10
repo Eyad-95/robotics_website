93246fbee468a48f00433953b9474fbef82d364e
Eyad Hussein <eyadhussein952018@gmail.com>
2023-10-10 03:46:57 +0300

    finished competitions page with responsiveness and edited nav collapse, organized html files, next step, organize the sass files and put comments

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const prevButton = document.getElementById("slide-arrow-prev");
 const nextButton = document.getElementById("slide-arrow-next");
 
 const toggleNavButton = document.getElementById("toggle-nav-button");
+const toggleNavLine = document.querySelector(".ham-btn__line");
 const navCollapsed = document.getElementById("nav-collapsed");
 const navCollapsedNavList = document.getElementById("nav-collapsed__nav-list");
 
@@ -11,26 +12,37 @@ const toggleNavButtonFunc = () => {
   toggleNavButton.classList.toggle("ham-btn--toggled");
 
   if (toggleNavButton.classList.contains("ham-btn--toggled")) {
-    toggleNavButton.style.position = "fixed";
+    if (toggleNavButton.classList.contains("helper-class--ham-btn")) {
+      toggleNavLine.setAttribute(
+        "style",
+        "transform: translate(-50%, 0%) rotate(45deg);"
+      );
+    }
     navCollapsed.style.transform = "translateX(0%)";
     navCollapsedNavList.style.transform = "translateX(0%)";
   } else {
-    toggleNavButton.style.position = "absolute";
+    if (toggleNavButton.classList.contains("helper-class--ham-btn")) {
+      toggleNavLine.setAttribute("style", "transform: translate(-50%, -50%);");
+    }
     navCollapsed.style.transform = "translateX(-100%)";
     navCollapsedNavList.style.transform = "translateX(-100%)";
   }
 };
 
 // index.html -- carousel functionality
-nextButton.addEventListener("click", () => {
-  const slideWidth = slide.clientWidth;
-  carouselContainer.scrollLeft += slideWidth;
-  console.log("he");
-});
-prevButton.addEventListener("click", () => {
-  const slideWidth = slide.clientWidth;
-  carouselContainer.scrollLeft -= slideWidth;
-});
+if (nextButton) {
+  nextButton.addEventListener("click", () => {
+    const slideWidth = slide.clientWidth;
+    carouselContainer.scrollLeft += slideWidth;
+  });
+}
+
+if (prevButton) {
+  prevButton.addEventListener("click", () => {
+    const slideWidth = slide.clientWidth;
+    carouselContainer.scrollLeft -= slideWidth;
+  });
+}
 
 // toggle of hambtn
 toggleNavButton.addEventListener("click", toggleNavButtonFunc);
